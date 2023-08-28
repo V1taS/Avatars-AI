0949e092568ed5954a137401bbf4a29e8550ec06
V1taS <SosinVitalii@iCloud.com>
2023-08-28 19:45:59 +0200

    Fix

--- Tuist/ProjectDescriptionHelpers/Project+Appearance.swift
+++ Tuist/ProjectDescriptionHelpers/Project+Appearance.swift
@@ -19,7 +19,7 @@ public let bundleTest = "com.sosinvitalii.avatarsTests"
 
 // Settings
 public let iOSversion = "16.0"
-public let provisioningProfileApp = "2fd577ab-3b9d-444c-8646-8b315aa34c4f"
+public let provisioningProfileApp = "ba147e2b-0800-4fc1-9510-9044c13e1da8"
 public let developmentTeam = "34VDSPZYU9"
 public let codeSignIdentity = "Apple Distribution: Vitalii Sosin (34VDSPZYU9)"
 public let schemesURL = "AvatarsAI"
